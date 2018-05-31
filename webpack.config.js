13b07dbc7a0305d05df350da11999e51cb5616fa
Shamim Akhtar <sakhtar@INNOMINDSBANG.COM>
2018-05-31 10:42:51 +0530

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports={
          headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+            "crossDomain":true,
+            "crossOrigin":true,
           },
          stats: 'minimal',
         
